33f957da16aa62d492713b453339374a3f45ebe3
carlmanuel <carllouismanuel09@gmail.com>
2021-06-09 09:40:18 +0800

    update

--- app/Http/Services/NexmoService.php
+++ app/Http/Services/NexmoService.php
@@ -19,13 +19,13 @@ class SendService
 
         $MESSAGE = $otp.' This is your G2GBox OTP valid for the next 5 minutes.';
 
-        if(config('app.env') == 'local') {
-            $user = User::where('mobile_number', $mobileNumber)->first();
-            Mail::raw($MESSAGE, function ($message) use($user) {
-                $message->to($user->email)->subject('OTP');
-            });
-        }
-        else {
+        // if(config('app.env') == 'local') {
+        //     $user = User::where('mobile_number', $mobileNumber)->first();
+        //     Mail::raw($MESSAGE, function ($message) use($user) {
+        //         $message->to($user->email)->subject('OTP');
+        //     });
+        // }
+        // else {
             $basic  = new \Nexmo\Client\Credentials\Basic( env('NEXMO_API_KEY'),  env('NEXMO_API_SECRET'));
             $client = new \Nexmo\Client($basic);
     
@@ -34,7 +34,7 @@ class SendService
                 'from' => 'G2GBox',
                 'text' => $MESSAGE
             ]);
-        }
+        // }
         return $otp;
     }
 
