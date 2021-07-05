e4d94dea0bd50e90d229170ae835fcc61150598e
carlmanuel <carllouismanuel09@gmail.com>
2021-07-05 23:30:38 +0800

    [B2B-API] update API for user results to add a membership type if they're premium, privilege, preferred

--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -19,6 +19,20 @@ class User extends JsonResource
             $data['roles'] = collect($data['user_roles'])->map(function($v){ return $v['role']; });
             unset($data['user_roles']);
         }
+        if(config('UCC.type') == 'RED') {
+            if(isset($data['vip_level'])) {
+                if($data['vip_level'] == 5 || $data['vip_level'] == 6) {
+                    $data['membership'] = 'Premium';
+                }
+                elseif($data['vip_level'] == 3 || $data['vip_level'] == 4) {
+                    $data['membership'] = 'Preferred';
+                }
+                elseif($data['vip_level'] == 1 || $data['vip_level'] == 2) {
+                    $data['membership'] = 'Privilege';
+                }
+                else $data['membership'] = 'Classic';
+            }
+        }
 
         return $data;
     }
