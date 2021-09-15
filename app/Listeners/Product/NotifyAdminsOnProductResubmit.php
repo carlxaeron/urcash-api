e68341c7de1334ce1836e90f2bcd7a13dfa3bb99
carlmanuel <carllouismanuel09@gmail.com>
2021-09-15 23:20:28 +0800

    || B2B - created new mail notification for resubmitted products and will send this email when product detects the resubmit

--- app/Listeners/Product/NotifyAdminsOnProductResubmit.php
+++ app/Listeners/Product/NotifyAdminsOnProductResubmit.php
@@ -3,6 +3,10 @@
 namespace App\Listeners\Product;
 
 use App\Events\Product\Resubmit;
+use App\Notifications\ProductResubmit;
+use App\Role;
+use App\User;
+use App\UsersRole;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
@@ -26,6 +30,15 @@ class NotifyAdminsOnProductResubmit
      */
     public function handle(Resubmit $event)
     {
+        $product = $event->product;
+
+        $role = Role::where('slug','administrator')->first();
+        $users = UsersRole::where('role_id', $role->id)->with(['user'])->get();
+        foreach($users as $user) {
+            $_user = User::find($user->user->id);
+            if($_user) $_user->notify(new ProductResubmit());
+        }
+        
         $product = $event->product;
     }
 }
